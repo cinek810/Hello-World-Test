3ce566ef35565a5b519211659563abb77bb16047
mikesalmonuk <mjsalmon@gmail.com>
2014-01-21 20:11:00 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -5,8 +5,8 @@ __addon__       = xbmcaddon.Addon(id='plugin.video.helloworld')
 __addonname__   = __addon__.getAddonInfo('name')
 __icon__        = __addon__.getAddonInfo('icon')
  
-title = "Hello World"
-text = "This is some text"
+title = "Hello World 2"
+text = "This is some text 2"
 time = 5000  # ms
  
 xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(title, text, time, __icon__))
